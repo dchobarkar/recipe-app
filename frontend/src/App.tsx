ae65d4115d8c891b640816cf12b6a3068621788e
Darshan Chobarkar <dchobarkar@gmail.com>
2024-06-03 12:10:39 +0530

    display search results on ui

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,38 @@
-import React from "react";
+import React, { FormEvent, useState } from "react";
+
+import * as api from "./api";
+import { Recipe } from "./types";
 import "./App.css";
 
-const App = () => <div>This is app</div>;
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("burger");
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
+
+  const handleSearchSubmit = async (event: FormEvent) => {
+    event.preventDefault();
+
+    try {
+      const recipes = await api.searchRecipes(searchTerm, 1);
+      setRecipes(recipes.results);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  return (
+    <div>
+      <form onSubmit={(event) => handleSearchSubmit(event)}>
+        <button type="submit">Submit</button>
+      </form>
+
+      {recipes.map((recipe) => {
+        <div>
+          recipe image location: {recipe.image}
+          recipe title: {recipe.title}
+        </div>;
+      })}
+    </div>
+  );
+};
 
 export default App;
